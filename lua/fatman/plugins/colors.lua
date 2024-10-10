8c2f09b14204974a25966cbf63806d8c9151cc91
Mauricio <111543689+Oiciraum@users.noreply.github.com>
2024-10-10 10:52:15 -0400

    Took out color pencils function

--- lua/fatman/plugins/colors.lua
+++ lua/fatman/plugins/colors.lua
@@ -1,11 +1,3 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-end
 
 return {
     {
@@ -21,7 +13,6 @@ return {
 
             vim.cmd("colorscheme rose-pine")
 
-            ColorMyPencils()
         end
     },
 
